4e3e32b5c1e6847ef33896e8aff73578369c06ef
dkdkv <dulustan1996@gmail.com>
2023-10-04 16:05:10 +0900

    feat: add pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,52 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import React from "react";
+import Head from "next/head";
+
+const APP_NAME = "Погода в Якутске";
+const APP_DEFAULT_TITLE = "Погода в Якутске";
+const APP_TITLE_TEMPLATE = "%s - Погода в Якутске";
+const APP_DESCRIPTION = "Погода в Якутске на 3 дня";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Погода в Якутске',
-  description: 'Погода в Якутске на 3 дня',
-  icons: 'favicon.ico',
+  icons: 'mask-icon.svg',
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  themeColor: "#FFFFFF",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 }
 
 export default function RootLayout({
@@ -17,6 +56,31 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+    <Head>
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <title>My awesome PWA app</title>
+      <meta name="description" content="Best PWA app in the world!" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="mask-icon" href="/icons/mask-icon.svg" color="#FFFFFF" />
+      <meta name="theme-color" content="#ffffff" />
+      <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+      <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/icons/touch-icon-ipad.png"
+      />
+      <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/touch-icon-iphone-retina.png"
+      />
+      <link
+          rel="apple-touch-icon"
+          sizes="167x167"
+          href="/icons/touch-icon-ipad-retina.png"
+      />
+      <link rel="manifest" href="/manifest.json" />
+    </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
